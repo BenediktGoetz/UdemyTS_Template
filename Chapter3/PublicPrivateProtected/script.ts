0e8d4f08c508147d22c33c4ab4546e086df9dfcf
Benedikt Götz <bgoetz@itemis.com>
2022-02-08 12:18:58 +0100

    PublicPrivateProtected

---
+++ Chapter3/PublicPrivateProtected/script.ts
@@ -0,0 +1,28 @@
+class Point2D {
+    protected xPos: number;
+    private yPos: number;
+    //readonly name?: string;
+
+    constructor(xPos: number = 0, yPos: number = 0) {
+        this.xPos = xPos;
+        this.yPos = yPos;
+    }
+}
+
+class Point3D extends Point2D {
+    public zPos: number;
+
+    constructor(xPos: number = 0, yPos: number = 0, zPos: number = 0) {
+        super(xPos, yPos);
+        this.zPos = zPos;
+    }
+}
+
+const p1 = new Point2D(0, 0);
+console.log(p1);
+
+const p2 = new Point2D();
+console.log(p2);
+
+const p3 = new Point3D(1, 1, 1);
+console.log(p3);
