e0557cf441c1eafc7e873c61f65dbe6581b664fb
Benedikt Götz <bgoetz@itemis.com>
2022-02-08 10:41:55 +0100

    Enum

---
+++ Chapter2/Enum/script.ts
@@ -0,0 +1,47 @@
+enum PermissionLevel {
+    STUDENT,
+    INSTRUCTOR,
+    ADMIN,
+}
+
+interface User {
+    name: string;
+    age: number;
+    courses: string[];
+}
+
+interface UserExtended extends User {
+    permissionLevel?: PermissionLevel;
+}
+
+const user1: UserExtended = {
+    name: 'Jan',
+    age: 27,
+    courses: ['C', 'C++', 'Python', 'TypeScript'],
+    permissionLevel: PermissionLevel.INSTRUCTOR,
+};
+
+const user2: UserExtended = {
+    name: 'Daniel',
+    age: 24,
+    courses: ['Java', 'C#'],
+};
+
+console.log(user1);
+console.log(user2);
+
+function printUser(user: User) {
+    console.log(user.name);
+    console.log(user.age);
+    console.log(user.courses);
+}
+
+printUser(user1);
+
+function printUserVerbose(user: User) {
+    console.log(user.name);
+    console.log(user.age);
+    console.log(user.courses);
+}
+
+printUser(user1);
